bd51579e0427cdf01c58c5349df1d7a56d52158f
TinyAngle Studio <tinyanglestudio@gmail.com>
2026-05-25 10:58:45 +0530

    Populate empty Next.js pages and routes with valid templates to fix build errors

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+﻿import { MetadataRoute } from 'next'
+ 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: 'https://acharya-sumit.vercel.app',
+      lastModified: new Date(),
+    },
+  ]
+}
